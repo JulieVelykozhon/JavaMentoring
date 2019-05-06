cf985c2e6612f2e8e4580ef62521b09e0e0177f3
Yuliia_Goncharuk <https://github.com/JulieVelykozhon/JavaMentoring.git>
2019-05-06 12:25:20 +0300

    Home Task 5.1

---
+++ src/hometask5/subTask1/Triangle.java
@@ -0,0 +1,29 @@
+package hometask5.subTask1;
+
+public class Triangle {
+    public static void main(String[] args) {
+       // try {
+            double x1 = 0.0,
+                    x2 = 3.0,
+                    x3 = 0.0;
+            double  y1 = 0.0,
+                    y2 = 0.0,
+                    y3 = 4.0;
+            double a = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+            double b = Math.sqrt((x1 - x3) * (x1 - x3) + (y1 - y3) * (y1 - y3));
+            double c = Math.sqrt((x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3));
+        //} catch (IllegalArgumentException e) {
+            if (a + b <= c || a + c <= b || b + c <= a)
+            System.out.println("Triangle does not exist");
+
+            else
+            {
+                double p = (a + b + c) / 2.0;
+                double square = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+                 System.out.println("Square: " + square);
+            }
+        }
+
+    }
+
+
